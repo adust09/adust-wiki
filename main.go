442e056a23cec6a297387a0de4dadd6137af6602
adust09 <shoukitsuda@icloud.com>
2024-09-17 23:49:56 +0900

    add todo api

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func main() {
+	dsn := "host=localhost user=postgres password=pass dbname=go_todo port=5432 sslmode=disable TimeZone=Asia/Tokyo"
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	db.AutoMigrate(&Todo{})
+
+	r := gin.Default()
+
+	r.POST("/todos", createTodo)
+	r.GET("/todos", getTodos)
+	r.GET("/todos/:id", getTodoByID)
+	r.PUT("/todos/:id", updateTodo)
+	r.DELETE("/todos/:id", deleteTodo)
+
+	r.Run()
+}
+
+type Todo struct {
+	ID     uint   `json:"id" gorm:"primaryKey"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
+}
+
+func createTodo(c *gin.Context) {
+	var todo Todo
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	db.Create(&todo)
+	c.JSON(http.StatusOK, todo)
+}
+
+func getTodos(c *gin.Context) {
+	var todos []Todo
+	db.Find(&todos)
+	c.JSON(http.StatusOK, todos)
+}
+
+func getTodoByID(c *gin.Context) {
+	var todo Todo
+	if err := db.First(&todo, c.Param("id")).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Todo not found"})
+		return
+	}
+	c.JSON(http.StatusOK, todo)
+}
+
+func updateTodo(c *gin.Context) {
+	var todo Todo
+	if err := db.First(&todo, c.Param("id")).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Todo not found"})
+		return
+	}
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	db.Save(&todo)
+	c.JSON(http.StatusOK, todo)
+}
+
+func deleteTodo(c *gin.Context) {
+	var todo Todo
+	if err := db.First(&todo, c.Param("id")).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Todo not found"})
+		return
+	}
+	db.Delete(&todo)
+	c.JSON(http.StatusOK, gin.H{"message": "Todo deleted"})
+}
